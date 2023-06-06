6c89aebb0b65c6f2e5fe8857966cf21d1447293f
chemecole <linkranma@gmail.com>
2023-06-06 13:43:16 +0200

    commit

--- scriptProduct.js
+++ scriptProduct.js
@@ -129,6 +129,7 @@ fetch(url)
     dropdown.appendChild(option);
   }
 
+  makeDropdown();
   //once the first product has been chosen, it would be more efficient to make it can only
   //get reduced (because the more you add ingredients, the less choice of TPs)
   //but because of the "remove" button, i think it 

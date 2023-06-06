4534ce170926290ff3c941d78bd2c05d84f3c387
chemecole <linkranma@gmail.com>
2023-06-06 09:35:05 +0200

    commit

---
+++ scriptProduct.js
@@ -0,0 +1,368 @@
+let dropdown = document.getElementById('chemistryDropdown');
+let selectedProductsSection = document.getElementById("selectedProducts");
+let availableTP = document.getElementById("availableTP")
+let availableCoolStuff = document.getElementById("availableCoolStuff")
+let defaultOption = document.createElement('option');
+let arrayUserInp = [];
+let btnAddProduct = document.getElementById('btnAddProduct')
+
+dropdown.selectedIndex = 0;
+
+const url ="http://localhost:3000/products"
+const urlTP ="http://localhost:3000/tp"
+const urlCool = "http://localhost:3000/cool"
+
+
+
+class Product {
+  constructor(id, name, strpic, location, quantity)
+ {
+    this.id = id;
+    this.name = name;
+    this.strpic = strpic;
+    this.location = location;
+    this.quantity = quantity;
+  }
+
+}
+
+class Experiment{
+  constructor(id, name,chemP){
+    this.id =id;
+    this.name = name;
+    this.chemP = chemP;
+  }
+}
+
+class CoolStuff{
+  constructor(id, name, imageOrVideo, chemP, subject, level, source, theme ){
+    this.id =id;
+    this.name = name;
+    this.imageOrVideo = imageOrVideo;
+    this.chemP = chemP;
+    this.subject = subject;
+    this.level = level;
+    this.source = source;
+    this.theme = theme;
+  }
+
+}
+
+let tp
+let data
+let dataCool
+
+window.onload = async function() {
+  
+tp = recoverDataTP()
+data = recoverDataProducts()
+dataCool = recoverDataCoolStuff()
+await new Promise(resolve => setTimeout(resolve, 3000));
+};
+
+
+//this is to create the dropdown list:
+fetch(url)  
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem. Status Code: ' + 
+          response.status);  
+        return;  
+      }
+      response.json().then(function(data) {  
+        let option;     
+      for (let i = 0; i < data.length; i++) {
+        
+        option = document.createElement('option');
+        option.text = data[i].name;
+        dropdown.appendChild(option);
+          
+      }    
+      });  
+    }
+  
+  )  
+  .catch(function(err) {  
+    console.error('Fetch Error -', err);  
+  });
+
+  function isValable(arrayUserInp,userInp){
+    let dejaSelected = false
+    let containedInData = false
+    for(i=0; i < arrayUserInp.length; i++){
+      if(arrayUserInp[i]===userInp){
+        alert('ce produit chimique est déjà selectionné')
+        dejaSelected = true
+      }
+    }
+    for(i=0; i< data.length;i++){
+        if(data[i].name === userInp){
+            containedInData = true
+        }
+        
+    }
+
+    if(containedInData === false){
+
+      alert("désolé ce produit n'est pas présent dans la liste de produits disponibles. Peut-être c'est présent sous un autre nom !")
+    }
+    let isValable = false
+    if(dejaSelected === false && containedInData === true){
+        isValable = true
+    }
+    return isValable
+  }
+
+
+  //this function allows to not have to use everywhere this damn fetch function
+  function recoverDataProducts (){
+
+    let dataProducts = []
+    fetch(url)  
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem. Status Code: ' + 
+          response.status);  
+        return;  
+      }
+      response.json().then(function(data) {  
+        
+        for(i=0; i<data.length; i++){
+          let Prod = new Product(data[i].id, data[i].name,data[i].strpic, data[i].location, data[i].quantity);
+          dataProducts.push(Prod);
+        }
+ });  
+    }
+  )  
+  .catch(function(err) {  
+    console.error('Fetch Error -', err);  
+  });
+
+  return dataProducts;
+  }
+
+
+  function recoverDataTP(){
+
+    let dataTP = []
+    fetch(urlTP)  
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem. Status Code: ' + 
+          response.status);  
+        return;  
+      }
+      response.json().then(function(data) {  
+        
+        for(i=0; i<data.length; i++){
+          let exp = new Experiment(data[i].id, data[i].name,data[i].chemP);
+          dataTP.push(exp);
+        }
+ });  
+    }
+  )  
+  .catch(function(err) {  
+    console.error('Fetch Error -', err);  
+  });
+
+  return dataTP;
+
+  }
+
+  function recoverDataCoolStuff (){
+
+    let dataCool = []
+    fetch(urlCool)  
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem. Status Code: ' + 
+          response.status);  
+        return;  
+      }
+      response.json().then(function(data) {  
+        
+        for(i=0; i<data.length; i++){
+          let fun = new CoolStuff(data[i].id, data[i].name,data[i].imageOrVideo, data[i].chemP,data[i].subject,data[i].level, data[i].source, data[i].theme);
+          dataCool.push(fun);
+        }
+ });  
+    }
+  )  
+  .catch(function(err) {  
+    console.error('Fetch Error -', err);  
+  });
+
+  return dataCool;
+  }
+
+
+
+  
+
+ 
+
+
+  //once the first product has been chosen, it would be more efficient to make it can only
+  //get reduced (because the more you add ingredients, the less choice of TPs)
+  //but because of the "remove" button, i think it 
+  //will be easier to forget about efficiency
+
+  function actualizeTP(selectedProducts){
+    availableTP.innerHTML = ""
+
+    //the reason we have to wait  is that fetch is an async function, so the code continues without
+    //the fetching being finished
+    //maybe that would be an argument in favor of doing everything inside
+    // the fetch function?
+    //Look up : fetch 
+    if(selectedProducts.length>0){
+    for(countTP =0; countTP < tp.length; countTP++){
+      //so tp[countTP] is one of the experiment of the tp database
+
+      let allProductsAreIn = true // for each TP we check that
+      for(countProducts = 0; countProducts < selectedProducts.length; countProducts++){
+        //so selectedProducts[countProducts] is one of the products inputed by user
+
+
+        let productIsIn =false
+        let iInExperiment = 0
+
+        //so tp[countTP].chemP[iInExperiment] is one of the ingredient required for this specific tp
+        while(productIsIn === false && iInExperiment < tp[countTP].chemP.length){
+          if(tp[countTP].chemP[iInExperiment] === selectedProducts[countProducts]){
+            productIsIn = true
+          }
+  
+
+          iInExperiment += 1
+        }
+
+        if(productIsIn ===false){
+          allProductsAreIn = false
+        }
+
+      }
+
+      //here we do the if allProductsAreIn is true stuff
+      if(allProductsAreIn){
+        availableTP.innerHTML += '<h3>' + tp[countTP].name +'</h3> <p> ingredients : ' + tp[countTP].chemP + '</p>'
+      }
+    }
+  }
+}
+
+
+
+function actualizeCoolStuff(selectedProducts){
+   availableCoolStuff.innerHTML = ""
+
+  //the reason we have to wait  is that fetch is an async function, so the code continues without
+  //the fetching being finished
+  //maybe that would be an argument in favor of doing everything inside
+  // the fetch function?
+  //Look up : fetch 
+  if(selectedProducts.length>0){
+    //should be COUNTCOOL BROO BUT to lazy to change the countTP (it's basically an i ++ thing right)
+  for(countTP =0; countTP < dataCool.length; countTP++){
+    //so tp[countTP] is one of the experiment of the tp database
+//or here rather dataCool[countTP] is one of the fun fact of the cool stuff database
+    let allProductsAreIn = true // for each TP we check that
+    for(countProducts = 0; countProducts < selectedProducts.length; countProducts++){
+      //so selectedProducts[countProducts] is one of the products inputed by user
+
+
+      let productIsIn =false
+      let iInExperiment = 0
+      //so tp[countTP].chemP[iInExperiment] is one of the ingredient required for this specific tp
+      while(productIsIn === false && iInExperiment < dataCool[countTP].chemP.length){
+        if(dataCool[countTP].chemP[iInExperiment] === selectedProducts[countProducts]){
+          productIsIn = true
+        }
+
+
+        iInExperiment += 1
+      }
+
+      if(productIsIn ===false){
+        allProductsAreIn = false
+      }
+
+    }
+
+    //here we do the if allProductsAreIn is true stuff
+    if(allProductsAreIn){
+      availableCoolStuff.innerHTML += '<h3>' + dataCool[countTP].name +'</h3> <p> ingredients : ' + dataCool[countTP].chemP + '</p>'
+    }
+  }
+}
+}
+
+
+
+function addProduct(arrayUserInp, userInp){
+  if(isValable(arrayUserInp, userInp)=== true){
+    arrayUserInp.push(userInp)
+  }
+return arrayUserInp
+}
+
+
+async function actualizeProducts(arrayUserInp){
+  //data = recoverDataProducts()
+  selectedProductsSection.innerHTML=""
+  //await new Promise(resolve => setTimeout(resolve, 200));
+
+  for(i=0; i<arrayUserInp.length;i++){
+    
+    let i2=0
+    let found= false
+    while(i2 < data.length && found === false){
+      if(data[i2].name == arrayUserInp[i]){
+      found=true
+      selectedProductsSection.innerHTML+='<div class = "oneSelectedProduct"><button class="btn-remove" onclick="btnClickedRemove(this)"></button><img src="source/images/chemicals/' + data[i2].strpic + ' " width="200px" ><h5>' + data[i2].name + '</h5><p>Emplacement : ' + data[i2].location + '</br> Quantité : '+ data[i2].quantity + '</p> </div>'
+    }
+    i2++
+  }
+
+  
+  }
+
+}
+  //check tutorials about arrows functions
+btnAddProduct.addEventListener('click',e => {
+  let userInp = document.getElementById("input").value.toLowerCase()
+  arrayUserInp = addProduct(arrayUserInp,userInp)
+  document.getElementById("input").value =""
+  actualizeTP(arrayUserInp)
+  actualizeProducts(arrayUserInp)
+  actualizeCoolStuff(arrayUserInp)
+})
+
+
+//btnRemoveProduct.addEventListener('click',e =>{
+  //var buttonClicked = event.target
+  //buttonClicked.parentElement.remove()
+//})
+
+
+function btnClickedRemove(get){
+  
+  var buttonClicked = event.target
+  var str = buttonClicked.parentElement.innerHTML
+  
+  var mySubString = str.substring(
+    str.indexOf("<h5>") + 4, 
+    str.lastIndexOf("</h5>") 
+);
+arrayUserInp = arrayUserInp.filter(e => e != mySubString);
+actualizeProducts(arrayUserInp)
+actualizeTP(arrayUserInp)
+actualizeCoolStuff(arrayUserInp)
+//buttonClicked.parentElement.remove()
+  
+}
+
---
+++ scriptTP.js
@@ -0,0 +1,281 @@
+const url ="http://localhost:3000/products"
+const urlTP ="http://localhost:3000/tp"
+
+
+class Experiment{
+  constructor(id, name, chemP, instructions, subject, level, source, theme){
+    this.id =id;
+    this.name = name;
+    this.chemP = chemP;
+    this.instructions = instructions;
+    this.subject = subject;
+    this.level = level;
+    this.source = source;
+    this.theme = theme;
+  }
+}
+
+class Product {
+  constructor(id, name, strpic, location, quantity)
+ {
+    this.id = id;
+    this.name = name;
+    this.strpic = strpic;
+    this.location = location;
+    this.quantity = quantity;
+  }
+
+}
+
+function recoverDataTP(){
+
+    let dataTP = []
+    fetch(urlTP)  
+    .then(  
+    function(response) {  
+        if (response.status !== 200) {  
+        console.warn('Looks like there was a problem. Status Code: ' + 
+            response.status);  
+        return;  
+        }
+        response.json().then(function(data) {  
+        
+        for(i=0; i<data.length; i++){
+       
+            let exp = new Experiment(data[i].id, data[i].name,data[i].chemP,data[i].instructions,data[i].subject,data[i].level,data[i].source,data[i].theme);
+            dataTP.push(exp);
+        }
+    });  
+    }
+    )  
+    .catch(function(err) {  
+    console.error('Fetch Error -', err);  
+    });
+
+    return dataTP;
+
+    }
+    
+    function recoverDataProducts (){
+
+      let dataProducts = []
+      fetch(url)  
+    .then(  
+      function(response) {  
+        if (response.status !== 200) {  
+          console.warn('Looks like there was a problem. Status Code: ' + 
+            response.status);  
+          return;  
+        }
+        response.json().then(function(data) {  
+          
+          for(i=0; i<data.length; i++){
+            let Prod = new Product(data[i].id, data[i].name,data[i].strpic, data[i].location, data[i].quantity);
+            dataProducts.push(Prod);
+          }
+   });  
+      }
+    )  
+    .catch(function(err) {  
+      console.error('Fetch Error -', err);  
+    });
+  
+    return dataProducts;
+    }
+  
+  
+   
+
+    let tp
+    let data
+
+    window.onload = async function() {
+      
+        tp = recoverDataTP()
+        data = recoverDataProducts()
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        
+        };
+
+        function change() {
+            var subjectCbs = document.querySelectorAll(".subject input[type='checkbox']");
+            var levelCbs = document.querySelectorAll(".level input[type='checkbox']");
+            var sourceCbs = document.querySelectorAll(".source input[type='checkbox']");
+            var themeCbs = document.querySelectorAll(".theme input[type='checkbox']");
+           
+
+            
+            var filters = {
+              subjectF: getClassOfCheckedCheckboxes(subjectCbs),
+              levelF: getClassOfCheckedCheckboxes(levelCbs),
+              sourceF: getClassOfCheckedCheckboxes(sourceCbs),
+              themeF: getClassOfCheckedCheckboxes(themeCbs)
+            };
+            filterResults(filters);
+          }
+          
+          function getClassOfCheckedCheckboxes(checkboxes) {
+            var classes = [];
+          
+            if (checkboxes && checkboxes.length > 0) {
+              for (var i = 0; i < checkboxes.length; i++) {
+                var cb = checkboxes[i];
+          
+                if (cb.checked) {
+                  classes.push(cb.getAttribute("rel"));
+                }
+              }
+            }
+            
+            return classes;
+          }
+          
+          function filterResults(filters) {
+            
+            //var rElems = document.querySelectorAll(".result div");
+            var hiddenElems = [];
+            for (var i = 0; i < tp.length; i++) {
+              var experiment = tp[i];
+              ///console.log("/n /n this experiment is"+tp[i].name)
+              
+              if (filters.subjectF.length > 0) {
+                var isHidden = true;
+          
+                for (var j = 0; j < filters.subjectF.length; j++) {
+                  var filter = filters.subjectF[j];
+                  if (experiment.subject.includes(filter)) {
+                    isHidden = false;
+                    break;
+                  }
+                
+                }
+          
+                if (isHidden) {
+                  hiddenElems.push(experiment);
+                }
+              }
+      
+             if (filters.levelF.length > 0) {
+                var isHidden = true;
+          
+                for (var j = 0; j < filters.levelF.length; j++) {
+                  var filter = filters.levelF[j];
+          
+                  if (experiment.level.includes(filter)) {
+                    isHidden = false;
+                    break;
+                  }
+                }
+          
+                if (isHidden) {
+                  hiddenElems.push(experiment);
+                }
+              }
+
+              
+             if (filters.sourceF.length > 0) {
+              var isHidden = true;
+        
+              for (var k = 0; k < filters.sourceF.length; k++) {
+                var filter = filters.sourceF[k];
+        
+                if (experiment.source.includes(filter)) {
+                  isHidden = false;
+                  break;
+                }
+              }
+        
+              if (isHidden) {
+                hiddenElems.push(experiment);
+              }
+            }
+
+            
+            if (filters.themeF.length > 0) {
+              var isHidden = true;
+        
+              for (var l = 0; l < filters.themeF.length; l++) {
+                var filter = filters.themeF[l];
+        
+                if (experiment.level.includes(filter)) {
+                  isHidden = false;
+                  break;
+                }
+              }
+        
+              if (isHidden) {
+                hiddenElems.push(experiment);
+              }
+            }
+            }
+            
+            
+            for (var i = 0; i < tp.length; i++) {
+            if(filters.subjectF =='' && filters.levelF == '' && filters.sourceF == '' && filters.themeF ==''){
+              document.getElementById("result").innerHTML=" "
+            }
+            else{
+
+              if(document.getElementById(tp[i].id) === null){
+              
+                let tpHTML = transformInHTML(tp[i])
+                document.getElementById("result").appendChild(tpHTML)
+                            
+                }
+                else{
+                  tpToShow =  document.getElementById(tp[i].id)
+                  tpToShow.style.display = "inline-block";
+                  tpToShow.parentElement.style.display="inline-block";
+                }
+
+            }
+            
+            }
+          
+            if (hiddenElems.length <= 0) {
+              return;
+            }
+            else{
+            for (var i3 = 0; i3 < hiddenElems.length; i3++) {
+              expToHide =document.getElementById(hiddenElems[i3].id)
+              expToHide.style.display = "none";
+              expToHide.parentElement.style.display = "none";
+              
+
+            }}
+          }
+
+
+          function transformInHTML(exp){
+            
+            divElement = document.createElement("div")
+            divElement.className = "oneTP";
+            divElement.innerHTML ='<div id="'+exp.id+'"><div class="expHeader"><h4>' + exp.name + '</h4></div><ul><li>Niveau: ' + exp.level + '</li><div class="tooltip-container"><li onmouseover="showLocation(this)" onmouseleave="hideLocation(this)">'+exp.chemP+'</li></div><a href="#" onclick="openPDF(this)" class="popup"><li>PDF Laborantin</li></a><li>PDF Élève</li></ul></div>'
+            return divElement
+          }
+
+          function showLocation(e){
+
+            let oneTPchemP = e.textContent.split(","); 
+            //console.log(e)
+            var tooltip= document.createElement('div')
+            tooltip.id="tooltip-text"
+            for(i=0; i<oneTPchemP.length;i++){
+              for(i2=0;i2<data.length;i2++){
+                if(oneTPchemP[i] == data[i2].name){
+                  tooltip.innerHTML += data[i2].name +": " +data[i2].location +"</br>"
+                  
+                }
+              }
+            }
+            e.after(tooltip)
+          }
+
+          function hideLocation(e){
+            document.getElementById("tooltip-text").remove()
+          }
+
+          function openPDF(e){
+            window.open('C:\\Users\\Progranma\\Downloads\\javascriptImages\\'+e.parentElement.parentElement.id +'.pdf', '_blank'); 
+            return false;
+          }

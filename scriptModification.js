4470cab084ecbbe954733c420964fd67d6d50c26
chemecole <linkranma@gmail.com>
2023-06-08 15:21:42 +0200

    commit

--- scriptModification.js
+++ scriptModification.js
@@ -1,2 +1,29 @@
 var formData = $("form.formTP").serializeObject();
 console.log(formData);
+
+window.onload = async function() {
+  
+    //tp = recoverDataTP()
+    //dataProducts = recoverDataProducts()
+    //dataCool = recoverDataCoolStuff()
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    makeDropdown();
+    };
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        // Accédez à la catégorie "products"
+        const products = data.products;
+        console.log("Category: products");
+        products.forEach(product => {
+          const productId = product.id;
+          const productName = product.name;
+          const productStrpic = product.strpic;
+          const productLocation = product.location;
+          const productQuantity = product.quantity;
+          prod = new Product(productId, productName, productStrpic, productLocation, productQuantity)
+          dataProducts.push(prod)
+        
+        });
+    

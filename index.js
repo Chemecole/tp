343e57167b4cb5444fd8fe5a715a54fe1d4e3102
chemecole <linkranma@gmail.com>
2023-06-22 11:34:15 +0200

    commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
+
 const router =  jsonServer.router("chemistry.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
